c012368e500b9960a6c417f768aaf490f0642d76
danger <danger@users.noreply.github.com>
2022-12-23 08:40:06 +0000

    Releasing danger-js version

--- danger-js.rb
+++ danger-js.rb
@@ -1,9 +1,21 @@
 class DangerJs < Formula
   homepage "https://github.com/danger/danger-js"
-  url "https://github.com/danger/danger-js/releases/download/11.2.0/danger-macos.zip"
-  sha256 "8f13366ae9c6521186919d79e03518035e5139705811ee0f476227dcd75b456a"
 
-  def install
-    bin.install "danger"
+  if Hardware::CPU.intel?
+    url "https://github.com/danger/danger-js/releases/download//danger-macos-x64.zip"
+    sha256 ""
+
+    def install
+      bin.install "danger-x64" => "danger"
+    end
+  end
+
+  if Hardware::CPU.arm?
+    url "https://github.com/danger/danger-js/releases/download//danger-macos-arm64.zip"
+    sha256 ""
+
+    def install
+      bin.install "danger-arm64" => "danger"
+    end
   end
 end
